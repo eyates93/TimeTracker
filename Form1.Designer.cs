9156a8c1b3ed2da97f87477a229d2b897b208139
Everett <everett.yates@atlapps.net>
2018-01-11 19:16:48 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace ATLTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StopWatchTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.TimeDateTimer = new System.Windows.Forms.Timer(this.components);
+            this.ClockIn = new System.Windows.Forms.Label();
+            this.lblClockIn = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(13, 263);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStop.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(13, 357);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 42);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.ForeColor = System.Drawing.Color.White;
+            this.lblSeconds.Location = new System.Drawing.Point(267, 194);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(46, 31);
+            this.lblSeconds.TabIndex = 4;
+            this.lblSeconds.Text = "00";
+            this.lblSeconds.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutes.ForeColor = System.Drawing.Color.White;
+            this.lblMinutes.Location = new System.Drawing.Point(208, 194);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(46, 31);
+            this.lblMinutes.TabIndex = 5;
+            this.lblMinutes.Text = "00";
+            // 
+            // lblHours
+            // 
+            this.lblHours.AutoSize = true;
+            this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.ForeColor = System.Drawing.Color.White;
+            this.lblHours.Location = new System.Drawing.Point(148, 194);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(46, 31);
+            this.lblHours.TabIndex = 6;
+            this.lblHours.Text = "00";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StopWatchTimer
+            // 
+            this.StopWatchTimer.Interval = 1000;
+            this.StopWatchTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(188, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(248, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 31);
+            this.label2.TabIndex = 9;
+            this.label2.Text = ":";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(143, 52);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(142, 65);
+            this.lblTime.TabIndex = 10;
+            this.lblTime.Text = "22:22";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecond.ForeColor = System.Drawing.Color.White;
+            this.lblSecond.Location = new System.Drawing.Point(270, 72);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(49, 40);
+            this.lblSecond.TabIndex = 11;
+            this.lblSecond.Text = "22";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.White;
+            this.lblDate.Location = new System.Drawing.Point(143, 117);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(140, 30);
+            this.lblDate.TabIndex = 12;
+            this.lblDate.Text = "JUNE 25 2016";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.ForeColor = System.Drawing.Color.White;
+            this.lblDay.Location = new System.Drawing.Point(289, 117);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(68, 30);
+            this.lblDay.TabIndex = 13;
+            this.lblDay.Text = "Friday";
+            // 
+            // TimeDateTimer
+            // 
+            this.TimeDateTimer.Interval = 1000;
+            this.TimeDateTimer.Tick += new System.EventHandler(this.TimeDateTimer_Tick);
+            // 
+            // ClockIn
+            // 
+            this.ClockIn.AutoSize = true;
+            this.ClockIn.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClockIn.ForeColor = System.Drawing.Color.White;
+            this.ClockIn.Location = new System.Drawing.Point(273, 262);
+            this.ClockIn.Name = "ClockIn";
+            this.ClockIn.Size = new System.Drawing.Size(51, 20);
+            this.ClockIn.TabIndex = 14;
+            this.ClockIn.Text = "-------";
+            // 
+            // lblClockIn
+            // 
+            this.lblClockIn.AutoSize = true;
+            this.lblClockIn.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClockIn.ForeColor = System.Drawing.Color.White;
+            this.lblClockIn.Location = new System.Drawing.Point(169, 262);
+            this.lblClockIn.Name = "lblClockIn";
+            this.lblClockIn.Size = new System.Drawing.Size(101, 20);
+            this.lblClockIn.TabIndex = 15;
+            this.lblClockIn.Text = "Clock In Time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(454, 411);
+            this.Controls.Add(this.lblClockIn);
+            this.Controls.Add(this.ClockIn);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximumSize = new System.Drawing.Size(470, 450);
+            this.MinimumSize = new System.Drawing.Size(400, 275);
+            this.Name = "Form1";
+            this.Text = "Stop Watch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer StopWatchTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Timer TimeDateTimer;
+        private System.Windows.Forms.Label ClockIn;
+        private System.Windows.Forms.Label lblClockIn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATLTime
+{
+    public partial class Form1 : Form
+    {
+        int seconds;
+        int minutes;
+        int hours;
+
+        public Form1()
+        {
+            InitializeComponent();
+            seconds = minutes = hours = 0;
+        }
+
+        private void lbTime_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Display for StopWatch
+        private void displayTime()
+        {
+            lblSeconds.Text = DateTime.Now.ToShortTimeString();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Stop Watch timer calculations
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+
+            if(seconds > 59)
+            {
+                minutes++;
+                seconds = 0;
+            }
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes = 0;
+            }
+
+            lblHours.Text = appendZero(hours);
+            lblMinutes.Text = appendZero(minutes);
+            lblSeconds.Text = appendZero(seconds);
+        }
+         //This is for the 00 effect on the stopwatch timer
+        private string appendZero(double str)
+        {
+            if (str <= 9)
+                return "0" + str;
+            else
+                return str.ToString();
+
+        }
+        //Start Button & Clock In Start
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StopWatchTimer.Start();
+            ClockIn.Text = DateTime.Now.ToString();
+        }
+        //Stop Button: all digits fall back to zero when pressed
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopWatchTimer.Stop();
+            seconds = minutes = hours = 0;
+            lblHours.Text = appendZero(hours);
+            lblMinutes.Text = appendZero(minutes);
+            lblSeconds.Text = appendZero(seconds);
+        }
+        //Pause Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StopWatchTimer.Stop();
+        }
+        //Clock time, seconds, date, day
+        private void TimeDateTimer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("HH:mm");
+            lblSecond.Text = DateTime.Now.ToString("ss");
+            lblDate.Text = DateTime.Now.ToString("MMM dd yyyy");
+            lblDay.Text = DateTime.Now.ToString("dddd");
+        }
+        //When program starts, TimeDateTimer is started
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeDateTimer.Start();
+        }
+    }
+}
